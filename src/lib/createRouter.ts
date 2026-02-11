dc222614f5523f21cefa451e7627c078bb18b12d
Andre Ximenes <andreximenes536@gmail.com>
2026-02-11 19:34:39 -0300

    Removido roles do createRouter para serem aplicados individualmente em cada rota

--- src/lib/createRouter.ts
+++ src/lib/createRouter.ts
@@ -1,22 +1,11 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
 import type { AppBindings } from "@/@types/declarations";
-import type { Roles } from "@/database/client/enums";
-import authMiddleware from "@/middlewares/auth";
 import { zodHook } from "@/middlewares/validator";
 
-type CreateRouterOptions = {
-	auth?: boolean;
-	roles?: Roles[];
-};
-
-export default function createRouter(options: CreateRouterOptions = {}) {
+export default function createRouter() {
 	const app = new OpenAPIHono<AppBindings>({
 		defaultHook: zodHook,
 	});
-
-	if (options.auth) {
-		app.use("/*", authMiddleware(options.roles || []));
-	}
 
 	return app;
 }
