78cf2ab8a4da2fa0d09306808829fb59efbea139
Andre Ximenes <andreximenes536@gmail.com>
2026-02-20 11:44:49 -0300

    chore(cors): permitir método PATCH

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -5,7 +5,7 @@ import environment from "@/lib/environment";
 
 const cors: MiddlewareHandler<AppBindings> = Cors({
 	origin: [environment.FRONTEND_URL, environment.S3_ENDPOINT_URL],
-	allowMethods: ["POST", "PUT", "GET", "DELETE"],
+	allowMethods: ["POST", "PATCH", "PUT", "GET", "DELETE"],
 	credentials: true,
 });
 
